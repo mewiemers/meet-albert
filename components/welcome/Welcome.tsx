350941808e6a61c9dd3263ce0a0ba98602ec73de
mewiemers <m.wiemers01@gmail.com>
2021-03-16 23:22:16 +0100

    add fonts to global

--- components/welcome/Welcome.tsx
+++ components/welcome/Welcome.tsx
@@ -7,12 +7,11 @@ function Logo() {
   return (
     <div className={styles.background}>
       <h1 className={styles.headline}>Meet</h1>
-      <Link href="#">
+      <Link href="/">
         <a>
           <img src="/logo-albert.svg" />
         </a>
       </Link>
-
       <h1 className={styles.headline}>Albert</h1>
     </div>
   );
