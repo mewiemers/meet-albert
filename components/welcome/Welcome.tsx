58480b3e9e538aef6890c225442e37dd91594220
mewiemers <m.wiemers01@gmail.com>
2021-03-16 18:34:06 +0100

    add Link Component

--- components/welcome/Welcome.tsx
+++ components/welcome/Welcome.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./Welcome.module.css";
+import Link from "next/link";
 
 // export type LogoProps = {};
 function Logo() {
   return (
     <div className={styles.background}>
       <h1 className={styles.headline}>Meet</h1>
-      <img src="/logo-albert.svg" />
+      <Link href="#">
+        <a>
+          <img src="/logo-albert.svg" />
+        </a>
+      </Link>
 
       <h1 className={styles.headline}>Albert</h1>
     </div>
