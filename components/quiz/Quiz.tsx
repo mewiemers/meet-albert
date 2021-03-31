29c95a13bb3508fda66dd8289eaef826d3d1e159
mewiemers <m.wiemers01@gmail.com>
2021-03-31 12:36:01 +0200

    💄 build the quiz frontpage
    - add some styles
    - set the special fonts

--- components/quiz/Quiz.tsx
+++ components/quiz/Quiz.tsx
@@ -2,30 +2,20 @@ import { QuizProps } from "../../utils/type";
 import styles from "./Quiz.module.css";
 import Quizbutton from "../quizbutton/Quizbutton";
 
-// const [currentQuestion, setCurrentQuestion] = useState(0);
-
-// const handleAnswerClick = (isCorrect) => {
-//   if (isCorrect) {
-//     alert("AMAZING");
-//   } else {
-//     alert("Hmm, try again!");
-//   }
-// };
-
 function Quiz({ question, answerOptions }: QuizProps) {
   const option = answerOptions.map((answer) => {
     return (
       <>
         <Quizbutton color="blue" label={answer.answerText} />
-        <Quizbutton color="red" label={answer.isCorrect} />
       </>
     );
   });
 
   return (
     <div className={styles.App}>
-      <h1>{question}</h1>
-      <div>{option}</div>
+      <h1 className={styles.headline}>QUIZ TIME</h1>
+      <h2>{question}</h2>
+      <div className={styles.btn}>{option}</div>
     </div>
   );
 }
