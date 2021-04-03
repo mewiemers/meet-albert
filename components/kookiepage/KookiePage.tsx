19f9bdb878042c07d19b12504ad6ea44d832fa9c
mewiemers <m.wiemers01@gmail.com>
2021-04-03 15:47:12 +0200

    ✨ add the detailspage for first animal

---
+++ components/kookiepage/KookiePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./KookiePage.module.css";
+
+function KookieDetail() {
+  return (
+    <div>
+      <img className={styles.background} src="/Kookiepage.svg" />
+      {/* <div className={styles.background}></div> */}
+    </div>
+  );
+}
+
+export default KookieDetail;
